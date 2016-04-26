f43c9760312393037c03ab18efa0204186a2c7ec
Javier Revuelta <javier.revult@gmail.com>
2016-04-26 14:47:02 -0500

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/items',         to: 'items#index',  as: 'items'
   get  '/items/new',    to: 'items#new',    as: "new_item"
   post '/items',        to: 'items#create'
-  get '/items/:id/edit' to: 'items#edit',   as: 'edit_item'
+  get '/items/:id/edit', to: 'items#edit',   as: 'edit_item'
   patch '/items/:id',   to: 'items#update'
   delete '/items/:id',  to: 'items#destroy'
   get '/items/:id',     to: 'itmes#shos',    as: 'item'
