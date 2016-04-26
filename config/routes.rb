c77448a2494e37fd27e7e0bef6e6c7fd79f846c6
JamesMarkWilton <james.mark.wilton@gmail.com>
2016-04-26 14:57:50 -0500

    Added a welcome controller and the routes for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  root 'items#index'
-  get '/items',         to: 'items#index',  as: 'items'
-  get  '/items/new',    to: 'items#new',    as: "new_item"
-  post '/items',        to: 'items#create'
-  get '/items/:id/edit', to: 'items#edit',   as: 'edit_item'
-  patch '/items/:id',   to: 'items#update'
-  delete '/items/:id',  to: 'items#destroy'
-  get '/items/:id',     to: 'itmes#shos',    as: 'item'
+  root 'welcome#index'
+  get "/auth/twitter",    to: "sessions#create" as: "login"
+  get "/auth/twitter/callback",  to: "sessions#create"
+  get '/items',           to: 'items#index',  as: 'items'
+  get  '/items/new',      to: 'items#new',    as: "new_item"
+  post '/items',          to: 'items#create'
+  get '/items/:id/edit',  to: 'items#edit',   as: 'edit_item'
+  patch '/items/:id',     to: 'items#update'
+  delete '/items/:id',    to: 'items#destroy'
+  get '/items/:id',       to: 'itmes#shos',    as: 'item'
 
 end
