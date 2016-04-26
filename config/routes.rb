fea527229ebf2240f42093425b57e6d462c239b1
Dongmin Lee <medi8600@gmail.com>
2016-04-26 15:58:09 -0500

    log in and out added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get "/auth/twitter",    to: "session#create", as: "login"
   get "/auth/twitter/callback",  to: "session#create"
+  get '/session', to: 'session#destroy'
+  delete '/session', to: 'session#destroy', as: 'logout'
   get '/items',           to: 'items#index',  as: 'items'
   get  '/items/new',      to: 'items#new',    as: "new_item"
   post '/items',          to: 'items#create'
