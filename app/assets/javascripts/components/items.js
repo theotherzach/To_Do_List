c271706da095b6f6cc5edeb0b7c2d2441c90d3ce
Zach Briggs <briggszj@gmail.com>
2016-04-28 07:54:20 -0500

    Finish video

--- app/assets/javascripts/components/items.js
+++ app/assets/javascripts/components/items.js
@@ -7,7 +7,32 @@
 
     data: function () {
       return {
+        newItem: {},
+        items: []
       }
+    },
+
+    init: function () {
+      // TODO: Fetch the collection of items
+    },
+
+    methods: {
+
+      handleNewItem: function () {
+        var self = this
+        // TODO: Bind template's form submit to this method
+        console.log("handleNewItem called")
+        $.ajax({
+          method: "POST",
+          url: "/api/items",
+          data: { item: self.newItem }
+        }).then(function (data) {
+          // Push the new item onto your items array
+          // Set the newItem to an empty object
+          console.log(data)
+        })
+      }
+
     }
 
   })
